ee5fb7ac8f03c96fcda41d0e72174488fd8e5e20
Ray <152318772+Tz-Ray@users.noreply.github.com>
2023-12-04 03:23:18 -0800

    Update cannon.h

--- cannon.h
+++ cannon.h
@@ -6,7 +6,7 @@ public:
     Cannon(bool isred) : Base(isred) {
         _name = "cannon";
         //same order as cart.
-        _captured_hierarchy = 2;
+        _captured_hierarchy = 1;
         //can capture anyone.
         _capturing_hierarchy = 9;
     }
