a505409a782234d78e0ee17accbfce2e0112abac
pcar5449 <146140461+pcar5449@users.noreply.github.com>
2023-12-05 23:01:06 -0800

    Update cart.h

--- cart.h
+++ cart.h
@@ -7,5 +7,12 @@ public:
         _name = "cart";
         _captured_hierarchy = 3;
         _capturing_hierarchy = 3;
+        redIcon.loadFromFile("Red Cart.png");
+        blackIcon.loadFromFile("Black Cart.png");
+        notRevTexture.loadFromFile("not rev.png");
+        redSprite.setTexture(redIcon);
+        blackSprite.setTexture(blackIcon);
+        notRevSprite.setTexture(notRevTexture);
     }
+
 };
