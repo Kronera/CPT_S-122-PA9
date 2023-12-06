9f30e7f162374ac6c4dda10ade4f230565848de9
pcar5449 <146140461+pcar5449@users.noreply.github.com>
2023-12-05 22:59:36 -0800

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -13,3 +13,20 @@ void Base::move(int x, int y) {
 Base::~Base() {
 
 }
+
+// getters
+int Base::getX() const {
+	return _x;
+}
+int Base::getY() const {
+	return _y;
+}
+
+
+// setters
+void Base::setX(int num) {
+	_x = num;
+}
+void Base::setY(int num) {
+	_y = num;
+}
