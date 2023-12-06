0819efa84faf4e4d05066aa0a4b5ca7e10830e42
pcar5449 <146140461+pcar5449@users.noreply.github.com>
2023-12-05 23:06:02 -0800

    Update king.h

--- king.h
+++ king.h
@@ -6,8 +6,15 @@ public:
     King(bool isred) : Base(isred) {
         _name = "King";
         //can capture anyone but pawn.
-        _captured_hierarchy = 8;
+        _captured_hierarchy = 10;
         //can only be captured by pawn.
-        _capturing_hierarchy = 8;
+        _capturing_hierarchy = 10;
+        redIcon.loadFromFile("Red King.png");
+        blackIcon.loadFromFile("Black King.png");
+        notRevTexture.loadFromFile("not rev.png");
+        redSprite.setTexture(redIcon);
+        blackSprite.setTexture(blackIcon);
+        notRevSprite.setTexture(notRevTexture);
+
     }
 };
