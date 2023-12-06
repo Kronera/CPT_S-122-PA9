7fbd327394930e2bb0833e6a603d544dc80f1585
pcar5449 <146140461+pcar5449@users.noreply.github.com>
2023-12-05 23:07:30 -0800

    Update pawn.h

--- pawn.h
+++ pawn.h
@@ -2,10 +2,17 @@
 #include "base.h"
 
 class Pawn : public Base {
-    public:
+public:
     Pawn(bool isred) : Base(isred) {
         _name = "pawn";
         _captured_hierarchy = 1;
         _capturing_hierarchy = 1;
+        redIcon.loadFromFile("Red Pawn.png");
+        blackIcon.loadFromFile("Black Pawn.png");
+        notRevTexture.loadFromFile("not rev.png");
+        redSprite.setTexture(redIcon);
+        blackSprite.setTexture(blackIcon);
+        notRevSprite.setTexture(notRevTexture);
+
     }
 };
