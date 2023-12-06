36c862765cac75db71c597af6d06579eb0a5da9a
pcar5449 <146140461+pcar5449@users.noreply.github.com>
2023-12-05 23:05:50 -0800

    Update horse.h

--- horse.h
+++ horse.h
@@ -7,5 +7,11 @@ public:
         _name = "horse";
         _captured_hierarchy = 2;
         _capturing_hierarchy = 2;
+        redIcon.loadFromFile("Red Horse.png");
+        blackIcon.loadFromFile("Black Horse.png");
+        notRevTexture.loadFromFile("not rev.png");
+        redSprite.setTexture(redIcon);
+        blackSprite.setTexture(blackIcon);
+        notRevSprite.setTexture(notRevTexture);
     }
 };
