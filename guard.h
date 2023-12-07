0cb8e15853b282d72471c435d661b11d6de2f8e0
Ray <152318772+Tz-Ray@users.noreply.github.com>
2023-12-06 16:09:52 -0800

    Update guard.h

--- guard.h
+++ guard.h
@@ -7,12 +7,15 @@ public:
         _name = "guard";
         _captured_hierarchy = 5;
         _capturing_hierarchy = 5;
+        loadtexture();
+    }
+
+    void loadtexture(void) override {
         blackIcon.loadFromFile("Black Guard.png");
         redIcon.loadFromFile("Red Guard.png");
         notRevTexture.loadFromFile("not rev.png");
         blackSprite.setTexture(blackIcon);
         redSprite.setTexture(redIcon);
         notRevSprite.setTexture(notRevTexture);
-
     }
 };
